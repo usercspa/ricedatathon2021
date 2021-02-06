617edcb0d6bc3eea0f55959e37946de47e5d702b
Your Name <you@example.comspacchbk@gmail.com>
2021-02-06 04:54:28 +0000

    load xlsx file instead of csv/txt

--- script.R
+++ script.R
@@ -5,9 +5,11 @@
 #Objective: Using the corpus of publications provided and any supplemental datasets of interest, identify key areas of concern or opportunity for the telemedicine industry. A successful project will be able to synthesize the results of analysis on the provided dataset in the context of the conditions of the COVID-19 pandemic. 
 #Dataset: https://zenodo.org/record/4031923#.YB4UYmiTJD9
 
-#load dataset
-data <- read.table("Bibliometrix-Export-File-telemedicine-trade_journal.txt", header=T, fill=TRUE)
-mydata <- read.table("Bibliometrix-Export-File-telemedicine-trade_journal - Sheet 1.csv", header=T, sep=";", fill=TRUE)
+#install packages
+install.packages("readxl")
 
-NAMES <- read.table("Bibliometrix-Export-File-telemedicine-trade_journal - Sheet 1.csv", nrow = 1, stringsAsFactors = FALSE, sep = ",")
-mydata <- read.table("Bibliometrix-Export-File-telemedicine-trade_journal - Sheet 1.csv", skip = 1, stringsAsFactors = FALSE, sep = ",", fill=TRUE)
+#load packages
+library("readxl")
+
+#load xlsx files
+myData <- read_excel("Bibliometrix-Export-File-telemedicine-trade_journal.xlsx")
